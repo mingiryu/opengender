c137641defbf5b156fa40ec0f2800cffe94d613a
mingiryu <mingi@opensyllabus.org>
2022-11-18 20:42:40 -0500

    simplify utils

--- opengender/dame_utils.py
+++ opengender/dame_utils.py
@@ -3,34 +3,33 @@ import unicodedata
 from opengender.paths import INTERALL_PATH
 
 
-class DameUtils:
-    def dicc_dataset(self):
-        return {"inter": INTERALL_PATH}
+def dicc_dataset():
+    return {"inter": INTERALL_PATH}
 
-    def drop_dots(self, s):
-        # given s removes dots symbols in the string
-        aux = ""
-        for c in unicodedata.normalize("NFD", str(s)):
-            if c != ".":
-                aux = aux + c
-        return aux
+def drop_dots(s):
+    # given s removes dots symbols in the string
+    aux = ""
+    for c in unicodedata.normalize("NFD", str(s)):
+        if c != ".":
+            aux = aux + c
+    return aux
 
-    def force_whitespaces(self, s):
-        # replace underscore, hyphens, ... by white spaces
-        aux = ""
-        for c in unicodedata.normalize("NFD", str(s)):
-            if (c == "_") or (c == "-"):
-                aux = aux + " "
-            else:
-                aux = aux + c
-        return aux
+def force_whitespaces(s):
+    # replace underscore, hyphens, ... by white spaces
+    aux = ""
+    for c in unicodedata.normalize("NFD", str(s)):
+        if (c == "_") or (c == "-"):
+            aux = aux + " "
+        else:
+            aux = aux + c
+    return aux
 
-    def drop_accents(self, s):
-        # given a string s delete accents
-        return "".join(
-            (
-                c
-                for c in unicodedata.normalize("NFD", s)
-                if unicodedata.category(c) != "Mn"
-            )
+def drop_accents(s):
+    # given a string s delete accents
+    return "".join(
+        (
+            c
+            for c in unicodedata.normalize("NFD", s)
+            if unicodedata.category(c) != "Mn"
         )
+    )
--- tests/test_dame_utils.py
+++ tests/test_dame_utils.py
@@ -1,22 +1,15 @@
 import unittest
-import collections
 
-from opengender.dame_utils import DameUtils
-
-
-collections.Callable = collections.abc.Callable
+from opengender.dame_utils import drop_accents, drop_dots, force_whitespaces
 
 
 class TddInPythonExample(unittest.TestCase):
     def test_drop_dots(self):
-        u = DameUtils()
-        self.assertEqual(1212, int(u.drop_dots(12.12)))
+        self.assertEqual(1212, int(drop_dots(12.12)))
 
     def test_force_whitespaces(self):
-        u = DameUtils()
-        self.assertEqual("JUAN DAVID", u.force_whitespaces("JUAN-DAVID"))
-        self.assertEqual("JUAN DAVID", u.force_whitespaces("JUAN_DAVID"))
+        self.assertEqual("JUAN DAVID", force_whitespaces("JUAN-DAVID"))
+        self.assertEqual("JUAN DAVID", force_whitespaces("JUAN_DAVID"))
 
     def test_drop_accents(self):
-        u = DameUtils()
-        self.assertEqual("Ines", u.drop_accents("Inés"))
+        self.assertEqual("Ines", drop_accents("Inés"))
