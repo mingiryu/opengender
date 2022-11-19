7e91c7a565a1d90a8c1acfb1bd2e04f9375a4d76
mingiryu <mingi@opensyllabus.org>
2022-11-18 21:01:33 -0500

    Hoist out all functions

--- opengender/dame_sexmachine.py
+++ opengender/dame_sexmachine.py
@@ -57,112 +57,113 @@ def features_list(path=ALL_PATH):
     return flist
 
 
-class DameSexmachine:
-    def csv2gender_list(self, path, *args, **kwargs):
-        # generating a list of 0, 1, 2 as females, males and unknows
-        # TODO: ISO/IEC 5218 proposes a norm about coding gender:
-        # ``0 as not know'',``1 as male'', ``2 as female''
-        # and ``9 as not applicable''
-        gender_column = kwargs.get("gender_column", 4)
-        gender_f_chars = kwargs.get("gender_f_chars", "f")
-        gender_m_chars = kwargs.get("gender_m_chars", "m")
-        delimiter = kwargs.get("delimiter", ",")
-        glist = []
-        with open(path) as csvfile:
-            sexreader = csv.reader(csvfile, delimiter=delimiter, quotechar='"')
-            next(sexreader, None)
-            gender = ""
-            for row in sexreader:
-                try:
-                    gender = row[gender_column]
-                except IndexError:
-                    print(
-                        "The method csv2gender_list has not row[%s]"
-                        % str(gender_column)
-                    )
-                    print("To review that gender row is set in the input")
-                    # os.kill(os.getpid(), signal.SIGUSR1)
-                if gender == gender_f_chars:
-                    g = 0
-                elif gender == gender_m_chars:
-                    g = 1
-                else:
-                    g = 2
-                glist.append(g)
-
-        return glist
-
-    def svc(self):
-        # Scikit svc classifier
-        X = np.array(features_list(path=ALL_PATH))
-        y = self.csv2gender_list(path=ALL_PATH)
-        clf = svm.SVC()
-        clf.fit(X, y)
-        filename = DATA_DIR / "svc_model.sav"
-        pickle.dump(clf, open(filename, "wb"))
-        return clf
-
-    def svc_load(self):
-        pkl_file = open(DATA_DIR / "svc_model.sav", "rb")
-        clf = pickle.load(pkl_file)
-        pkl_file.close()
-        return clf
-
-    def forest(self):
-        # Scikit forest classifier
-        X = np.array(features_list(path=ALL_PATH))
-        y = np.array(self.csv2gender_list(path=ALL_PATH))
-        X, y = make_classification(
-            n_samples=7000,
-            n_features=33,
-            n_informative=33,
-            n_redundant=0,
-            random_state=0,
-            shuffle=False,
-        )
-        rf = RandomForestRegressor(n_estimators=20, random_state=0)
-        rf.fit(X, y)
-        filename = DATA_DIR / "forest_model.sav"
-        pickle.dump(rf, open(filename, "wb"))
-        return rf
-
-    def forest_load(self):
-        pkl_file = open(DATA_DIR / "forest_model.sav", "rb")
-        clf = pickle.load(pkl_file)
-        pkl_file.close()
-        return clf
-
-    def guess(self, name, binary=False, ml="svc"):
-        # guess method to check names dictionary and nltk classifier
-        # TODO: ISO/IEC 5218 proposes a norm about coding gender:
-        # ``0 as not know'',``1 as male'', ``2 as female''
-        # and ``9 as not applicable''
-        guess = 2
-
-        vector = features_int(name)
-        if (guess == "unknown") | (guess == 2):
-            vector = list(features_int(name).values())
-            if ml == "svc":
-                m = self.svc_load()
-                predicted = m.predict([vector])
-                guess = predicted[0]
-            elif ml == "forest":
-                m = self.forest_load()
-                predicted = m.predict([vector])
-                guess = predicted[0]
-
-            if binary:
-                if guess == "female":
-                    guess = 0
-                elif guess == "male":
-                    guess = 1
-                elif guess == "unkwnon":
-                    guess = 2
+def csv2gender_list(path):
+    # generating a list of 0, 1, 2 as females, males and unknows
+    # TODO: ISO/IEC 5218 proposes a norm about coding gender:
+    # ``0 as not know'',``1 as male'', ``2 as female''
+    # and ``9 as not applicable''
+    gender_column = 4
+    gender_f_chars = "f"
+    gender_m_chars = "m"
+    glist = []
+    with open(path) as csvfile:
+        sexreader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        next(sexreader, None)
+        gender = ""
+        for row in sexreader:
+            try:
+                gender = row[gender_column]
+            except IndexError:
+                print(
+                    "The method csv2gender_list has not row[%s]"
+                    % str(gender_column)
+                )
+                print("To review that gender row is set in the input")
+                # os.kill(os.getpid(), signal.SIGUSR1)
+            if gender == gender_f_chars:
+                g = 0
+            elif gender == gender_m_chars:
+                g = 1
             else:
-                if guess == 0:
-                    guess = "female"
-                elif guess == 1:
-                    guess = "male"
-                elif guess == 2:
-                    guess = "unknown"
-        return guess
+                g = 2
+            glist.append(g)
+
+    return glist
+
+
+def svc():
+    X = np.array(features_list(path=ALL_PATH))
+    y = csv2gender_list(path=ALL_PATH)
+    clf = svm.SVC()
+    clf.fit(X, y)
+    filename = DATA_DIR / "svc_model.sav"
+    pickle.dump(clf, open(filename, "wb"))
+    return clf
+
+
+def svc_load():
+    pkl_file = open(DATA_DIR / "svc_model.sav", "rb")
+    clf = pickle.load(pkl_file)
+    pkl_file.close()
+    return clf
+
+
+def forest():
+    X = np.array(features_list(path=ALL_PATH))
+    y = np.array(csv2gender_list(path=ALL_PATH))
+    X, y = make_classification(
+        n_samples=7000,
+        n_features=33,
+        n_informative=33,
+        n_redundant=0,
+        random_state=0,
+        shuffle=False,
+    )
+    rf = RandomForestRegressor(n_estimators=20, random_state=0)
+    rf.fit(X, y)
+    filename = DATA_DIR / "forest_model.sav"
+    pickle.dump(rf, open(filename, "wb"))
+    return rf
+
+
+def forest_load():
+    pkl_file = open(DATA_DIR / "forest_model.sav", "rb")
+    clf = pickle.load(pkl_file)
+    pkl_file.close()
+    return clf
+
+
+def guess(name, binary=False, ml="svc"):
+    # guess method to check names dictionary and nltk classifier
+    # TODO: ISO/IEC 5218 proposes a norm about coding gender:
+    # ``0 as not know'',``1 as male'', ``2 as female''
+    # and ``9 as not applicable''
+    guess = 2
+
+    vector = features_int(name)
+    if (guess == "unknown") | (guess == 2):
+        vector = list(features_int(name).values())
+        if ml == "svc":
+            m = svc_load()
+            predicted = m.predict([vector])
+            guess = predicted[0]
+        elif ml == "forest":
+            m = forest_load()
+            predicted = m.predict([vector])
+            guess = predicted[0]
+
+        if binary:
+            if guess == "female":
+                guess = 0
+            elif guess == "male":
+                guess = 1
+            elif guess == "unkwnon":
+                guess = 2
+        else:
+            if guess == 0:
+                guess = "female"
+            elif guess == 1:
+                guess = "male"
+            elif guess == 2:
+                guess = "unknown"
+    return guess
--- tests/test_dame_sexmachine.py
+++ tests/test_dame_sexmachine.py
@@ -1,19 +1,18 @@
 import unittest
 import numpy as np
-import os.path
-import collections
 
-from opengender.dame_sexmachine import DameSexmachine, features_int
-from opengender.paths import DATA_DIR, ALL_PATH, PARTIAL_PATH
-
-
-collections.Callable = collections.abc.Callable
+from opengender.dame_sexmachine import (
+    features_int,
+    guess,
+    forest_load,
+    svc_load,
+    csv2gender_list,
+)
+from opengender.paths import PARTIAL_PATH
 
 
 class TddInPythonExample(unittest.TestCase):
-
     def test_sexmachine_features_int(self):
-        s = DameSexmachine()
         dicc = features_int("David")
         self.assertEqual(chr(dicc["last_letter"]), "d")
         self.assertEqual(chr(dicc["first_letter"]), "d")
@@ -32,37 +31,31 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(dicc["first_letter_vocal"], 0)
         self.assertEqual(dicc["last_letter_vocal"], 0)
         self.assertTrue(len(dicc.values()) > 30)
-    
+
     def test_dame_sexmachine_guess(self):
-        s = DameSexmachine()
-        self.assertEqual(s.guess("David"), "male")
-        self.assertEqual(s.guess("Laura"), "female")
-        self.assertEqual(s.guess("David", binary=True), 1)
-        self.assertEqual(s.guess("Laura", binary=True), 0)
-        self.assertEqual(s.guess("David", binary=True, ml="svc"), 1)
-        self.assertEqual(s.guess("Laura", binary=True, ml="svc"), 0)
-        self.assertEqual(s.guess("Palabra", binary=True, ml="svc"), 1)
-        self.assertEqual(s.guess("Nadiccionaria", binary=True), 0)
-        self.assertEqual(s.guess("Nadiccionaria"), "female")
+        self.assertEqual(guess("David"), "male")
+        self.assertEqual(guess("Laura"), "female")
+        self.assertEqual(guess("David", binary=True), 1)
+        self.assertEqual(guess("Laura", binary=True), 0)
+        self.assertEqual(guess("David", binary=True, ml="svc"), 1)
+        self.assertEqual(guess("Laura", binary=True, ml="svc"), 0)
+        self.assertEqual(guess("Palabra", binary=True, ml="svc"), 1)
+        self.assertEqual(guess("Nadiccionaria", binary=True), 0)
+        self.assertEqual(guess("Nadiccionaria"), "female")
         #        With accents:
-        self.assertEqual(s.guess("Inés"), "female")
+        self.assertEqual(guess("Inés"), "female")
         #        Without accents:
-        self.assertEqual(s.guess("Ines"), "female")
+        self.assertEqual(guess("Ines"), "female")
 
     def test_dame_gender_csv2gender_list(self):
-        s = DameSexmachine()
-        gl = s.csv2gender_list(path=PARTIAL_PATH)
+        gl = csv2gender_list(path=PARTIAL_PATH)
         self.assertEqual(
             gl, [1, 1, 1, 1, 2, 1, 0, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1]
         )
         self.assertEqual(len(gl), 21)
-        self.assertEqual(s.females, 3)
-        self.assertEqual(s.males, 16)
-        self.assertEqual(s.unknown, 2)
 
     def test_sexmachine_forest_load(self):
-        s = DameSexmachine()
-        m = s.forest_load()
+        m = forest_load()
         predicted = m.predict(
             [
                 [
@@ -106,8 +99,7 @@ class TddInPythonExample(unittest.TestCase):
         self.assertEqual(predicted, a)
 
     def test_sexmachine_svc_load(self):
-        s = DameSexmachine()
-        m = s.svc_load()
+        m = svc_load()
         predicted = m.predict(
             [
                 [
