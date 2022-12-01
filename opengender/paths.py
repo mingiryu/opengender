d4102a995f6d39bf994aac348c54b76a22e4f2ae
mingiryu <mingi@opensyllabus.org>
2022-11-30 22:26:50 -0500

    Update training set up

--- opengender/paths.py
+++ opengender/paths.py
@@ -3,7 +3,4 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).parent.parent / "data"
 
-CLF_PATH = DATA_DIR / 'clf.pkl'
-
-INTERALL_PATH = DATA_DIR / "interall.csv"
-ALL_PATH = DATA_DIR / "all.csv"
+CLF_PATH = DATA_DIR / "clf.pkl"
--- opengender/train.py
+++ opengender/train.py
@@ -1,69 +1,35 @@
 import pickle
-import numpy as np
-import pandas as pd
 
 from loguru import logger
 
-from sklearn import metrics
+from sklearn.metrics import make_scorer
 from sklearn.experimental import enable_halving_search_cv  # noqa
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import HalvingRandomSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC, LinearSVC
 
-from opengender.paths import ALL_PATH, CLF_PATH, INTERALL_PATH
-
+from opengender import metrics
+from opengender.paths import CLF_PATH
+from opengender.dataset import (
+    load_name_gender_inference,
+    load_damegender,
+    load_wiki_gendersort,
+)
 
 RANDOM_STATE = 7
 
 
-def encode_train(row):
-    if row.male == row.female:
-        return "unknown"
-    if row.male > row.female:
-        return "male"
-    else:
-        return "female"
-
-
-def encode_test(x):
-    if x.gender == "m":
-        return "male"
-    if x.gender == "f":
-        return "female"
-    else:
-        return "unknown"
-
-
-def load_dataset(train_path=INTERALL_PATH, test_path=ALL_PATH, sample_size=400_000):
-    train = pd.read_csv(train_path)
-    train = train[train.name.notna()]
-
-    train["X"] = train.name.str.lower()
-    train["y"] = train.apply(encode_train, axis=1)
-    train = train[train.y != "unknown"]
-    train = train.sample(sample_size, random_state=RANDOM_STATE)
-
-    test = pd.read_csv(test_path)
-    test = test[test.first_name.notna()]
-
-    test["X"] = test.first_name.str.lower()
-    test["y"] = test.apply(encode_test, axis=1)
-    test = test[test.y != "unknown"]
-
-    return test, train
-
-
 def main():
-    train, test = load_dataset()
+    train = load_name_gender_inference()
+    test = load_name_gender_inference()
 
     tv = TfidfVectorizer(
         analyzer="char",
         # max_df=0.75,
         # min_df=0.01,
-        # max_features=50_000,
-        # ngram_range=(1, 3),
-        # strip_accents="unicode",
+        # max_features=10_000,
+        ngram_range=(1, 4),
     )
     clf = LinearSVC(
         # kernel="linear",
@@ -77,19 +43,18 @@ def main():
     pipeline = Pipeline([("tv", tv), ("clf", clf)])
 
     params = {
-        "tv__strip_accents": ("unicode", "ascii"),
-        "tv__max_features": np.linspace(10_000, 100_000, 10, dtype="int"),
-        "tv__max_df": (0.5, 0.75, 0.9),
-        "tv__min_df": (0.1, 0.01, 0.001),
-        "tv__ngram_range": ((1, 2), (1, 3), (1, 4)),
-        "clf__C": (1, 10, 100),
-        "clf__tol": (1e-4, 1e-5, 1e-6),
+        # "tv__max_features": np.linspace(10_000, 100_000, 10, dtype="int"),
+        # "tv__max_df": (0.5, 0.75, 0.9),
+        # "tv__min_df": (0.1, 0.01, 0.001),
+        # "tv__ngram_range": ((1, 2), (1, 3), (1, 4)),
+        # "clf__C": (1, 10, 100),
+        # "clf__tol": (1e-4, 1e-5, 1e-6),
     }
 
     model = HalvingRandomSearchCV(
         pipeline,
         params,
-        scoring="f1_weighted",
+        scoring=make_scorer(metrics.error_coded, greater_is_better=False),
         verbose=3,
     )
 
@@ -101,12 +66,14 @@ def main():
 
     preds = model.predict(test.X)
 
-    f1 = metrics.f1_score(test.y, preds, average="weighted")
-    accuracy = metrics.accuracy_score(test.y, preds)
-    report = metrics.classification_report(test.y, preds, output_dict=True)
-
-    logger.info(dict(f1=f1, accuracy=accuracy))
-    logger.info(report)
+    logger.info(
+        dict(
+            error_coded=metrics.error_coded(test.y, preds),
+            error_coded_without_na=metrics.error_coded_without_na(test.y, preds),
+            na_coded=metrics.na_coded(test.y, preds),
+            error_gender_bias=metrics.error_gender_bias(test.y, preds),
+        )
+    )
 
     with open(CLF_PATH, "wb") as fh:
         pickle.dump(model, fh)
